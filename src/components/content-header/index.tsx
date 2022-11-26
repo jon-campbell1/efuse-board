8431b724ebd9be26cf7490006a4d82b93f57b3cf
Jonathan Campbell <campbell@RR19-MB90.local>
2022-11-26 13:43:47 -0500

    minor scss fix for post/comment uernames

--- src/components/content-header/index.tsx
+++ src/components/content-header/index.tsx
@@ -21,7 +21,7 @@ const ContentHeader = ({
         <div className="content-user-info">
             <img alt="profile pic" className={`${type}-profile-pic`} src="/assets/profile_pic.png"/>
             <div style={{marginLeft: 16}}>
-                <h3 className="content-username">{username}</h3>
+                <h3 className={`content-username ${type}-username`}>{username}</h3>
                 <span className="content-time">{timeAgo.format(new Date(timeStamp))}</span>
             </div>
         </div>
