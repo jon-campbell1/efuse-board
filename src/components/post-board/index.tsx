32f11445ab55b486032c8e9ef66b80298cc285dd
Jonathan Campbell <campbell@RR19-MB90.local>
2022-11-26 03:30:54 -0500

    small code cleanup, added styles

--- src/components/post-board/index.tsx
+++ src/components/post-board/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import PostCreator from '../post-creator';
 import Post from '../post';
 import { UserContext } from '../../contexts/UserContext';
